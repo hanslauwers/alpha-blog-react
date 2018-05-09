0bb41c20380f28c11a7b83918db0f89de9ce630f
Hans Lauwers <hans.lauwers@hotmail.com>
2018-05-09 18:51:46 +0000

    Add regex validation for hex-value of color

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  VALID_COLORHEX_REGEX = /\A#([A-F,0-9]){3,6}\Z/i
+  validates :color, format: { with: VALID_COLORHEX_REGEX }
   has_secure_password
+
 end
